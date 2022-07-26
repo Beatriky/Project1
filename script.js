5f6cbf773c1ceeb10fa38802a683646094e2dd02
Beatriky <beamaria14@yahoo.com>
2022-07-26 10:06:47 +0300

    adaugat event listener

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
- function onClick() {
-    alert("Day:  " + this.innerText);
-   // const data=event.target.dataset.date;
-  // document.getElementById("dataProg").innerText = data;
-     document.getElementById("dataProg").innerText = `Schedule for July ${this.innerText}, 2022`;
 
+
+function onClick() {
+    alert("Day:  " + this.innerText);
+
+   // document.getElementById("schedule").innerText = dataText;
+   // document.getElementById("schedule").innerText = `Schedule for July ${this.innerText}, 2022`;
  }
 
-console.log("ss");
+
+const daysVar=document.querySelectorAll('.days');
+daysVar.forEach(item=>
+{
+    item.addEventListener('click',event=>
+    {
+       // console.log(event.target.dataset.date, event.target.innerText, "Weekend: "+event.currentTarget.classList.contains(('weekdays'),));
+        const dataText=event.target.dataset.date;
+      //  document.getElementById("schedule").innerText = dataText;
+        document.getElementById("schedule").innerText = "Schedule for July "+dataText+" 2022";
+    })
+})
+
+
 let tableData = document.querySelectorAll("td");
+
 console.log(tableData);
+
 for(const days of tableData)
 {
     days.addEventListener("click", onClick);
     days.addEventListener("click", getinnerText);
 }
+
+
  function getinnerText() {
      let text = document.getElementById("user1").innerText;
 
